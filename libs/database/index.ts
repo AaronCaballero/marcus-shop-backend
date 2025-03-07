ee9d3e374c75557a8c1f00ca16291660c663f5f2
Aaron Caballero <caballero.aaron@protonmail.com>
2025-03-07 15:40:27 +0100

    Modify database library

--- libs/database/index.ts
+++ libs/database/index.ts
@@ -1,3 +1,4 @@
+export { TimestampableDto } from './dto/timestampable.dto';
 export { NumericTransformer } from './entities/numeric.transformer';
-export { isTimestampableEntity } from './entities/timestampable.entity';
+export { TimestampableEntity } from './entities/timestampable.entity';
 export { PostgresModule } from './postgres/postgres.module';
