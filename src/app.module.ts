409d155bae98a34df1f7cd4fa4aa1b5e87869940
Aaron Caballero <caballero.aaron@protonmail.com>
2025-03-13 23:14:52 +0100

    Remove redundant system files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostgresModule } from 'libs/database';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), PostgresModule, ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
