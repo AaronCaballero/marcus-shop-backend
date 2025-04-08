880a6509bde148795cc6207fcd95dc625952b5f8
Aaron Caballero <caballero.aaron@protonmail.com>
2025-04-08 14:08:02 +0200

    Import EventsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
+import { ValidationPipe } from 'libs/class-validator';
 import { PostgresModule } from 'libs/database';
-import { ValidationPipe } from '../libs/class-validator';
+import { EventsModule } from 'libs/event-emitter/event-emitter.module';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PostgresModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PostgresModule,
+    EventsModule,
+    ProductModule,
+  ],
   controllers: [],
   providers: [
     {
