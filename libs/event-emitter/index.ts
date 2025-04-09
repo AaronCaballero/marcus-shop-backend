eb5fba526d8c0c15bc81f9ea2617770fe92767f0
Aaron Caballero <caballero.aaron@protonmail.com>
2025-04-09 10:23:28 +0200

    Add event-emitter index

---
+++ libs/event-emitter/index.ts
@@ -0,0 +1,5 @@
+export {
+  CustomizationsByTypeProcessedEvent,
+  ProductFetchedEvent,
+} from './events/product.event';
+export { ProductCustomizationEventHandler } from './handlers/product-event.handler';
